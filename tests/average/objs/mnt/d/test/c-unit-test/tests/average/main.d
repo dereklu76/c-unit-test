d8e9aaaa549d75ffe6653504930d592ef4a76e48
Derek QQ <dereklu76@qq.com>
2024-10-19 19:08:43 +0800

    Docker image built on the github

---
+++ tests/average/objs/mnt/d/test/c-unit-test/tests/average/main.d
@@ -0,0 +1,38 @@
+objs//mnt/d/test/c-unit-test/tests/average/main.o: \
+ /mnt/d/test/c-unit-test/tests/average/main.cpp \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/CppUTestConfig.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/CppUTestGeneratedConfig.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/StandardCLibrary.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/CommandLineTestRunner.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestHarness.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/Utest.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/SimpleString.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/UtestMacros.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestResult.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestFailure.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestPlugin.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakWarningPlugin.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestOutput.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/CommandLineArguments.h \
+ /mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestFilter.h
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/CppUTestConfig.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/CppUTestGeneratedConfig.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/StandardCLibrary.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/CommandLineTestRunner.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestHarness.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/Utest.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/SimpleString.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/UtestMacros.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestResult.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestFailure.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestPlugin.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakWarningPlugin.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestOutput.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/CommandLineArguments.h:
+/mnt/d/test/c-unit-test/tools/cpputest/include/CppUTest/TestFilter.h:
